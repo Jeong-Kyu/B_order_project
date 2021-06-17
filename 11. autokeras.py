09b66a5b771c01f0f33078207a4634e34c36a8ec
Jeong-Kyu <jare92n@gmail.com>
2021-06-17 17:17:02 +0900

    210617 pytorch

---
+++ 11. autokeras.py
@@ -0,0 +1,69 @@
+import numpy as np
+import db_connect as db
+import pandas as pd
+import timeit
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Dense, Flatten, Dropout,LSTM, Conv2D,Input,Activation, LSTM, Dropout, BatchNormalization
+from tensorflow.keras.models import Sequential, Model, load_model
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split, KFold
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam, Adadelta, Adamax, Adagrad
+from tensorflow.keras.optimizers import RMSprop, SGD, Nadam
+from tensorflow.keras.activations import tanh, relu, elu, selu, swish
+from sklearn.metrics import mean_absolute_error as mae_
+from sklearn.metrics import mean_squared_error as mse_
+import tensorflow.keras.backend as K
+import autokeras as ak
+# db 직접 불러오기 
+def load_data(query, is_train = True):
+    query = query
+    db.cur.execute(query)
+    dataset = np.array(db.cur.fetchall())
+
+    # pandas 넣기
+    column_name = ['date', 'year', 'month', 'day', 'time', 'category', 'dong','temperature','rain','wind','humidity','person', 'value']
+    df = pd.DataFrame(dataset, columns=column_name)
+    db.connect.commit()
+
+    # pred = df.iloc[:,1:-1]
+
+    if is_train == True:
+        # train, test 나누기
+        train_value = df[ '2020-09-01' > df['date'] ]
+        x = train_value.iloc[:,1:-1].astype('float64')
+        y = train_value.iloc[:,-1].astype('float64').to_numpy()
+    else:
+        test_value = df[df['date'] >=  '2020-09-01']
+        x = test_value.iloc[:,1:-1].astype('float64')
+        y = test_value.iloc[:,-1].astype('float64').to_numpy()
+
+    
+    # 원 핫으로 컬럼 추가해주는 코드!!!!!    
+    x = pd.get_dummies(x, columns=["category", "dong"]).to_numpy()
+    # 카테고리랑 동만 원핫으로 해준다 
+
+    return x, y
+
+x_pred, y_pred = load_data("SELECT d.date,YEAR,MONTH, d.day, d.time, category, dong,temperature,rain,wind,humidity, IFNULL( c_person,0) AS person, VALUE FROM main_data_table AS d INNER JOIN `weather` AS s ON d.date = s.DATE AND d.time = s.time LEFT JOIN `covid19_re` AS c ON c.date = d.date ", is_train = False)
+x_train, y_train = load_data("SELECT d.date,YEAR,MONTH, d.day, d.time, category, dong,temperature,rain,wind,humidity, IFNULL( c_person,0) AS person, VALUE FROM main_data_table AS d INNER JOIN `weather` AS s ON d.date = s.DATE AND d.time = s.time LEFT JOIN `covid19_re` AS c ON c.date = d.date WHERE (d.TIME != 2 AND d.TIME != 3 AND d.TIME != 4 AND d.TIME != 5  AND d.TIME != 6 AND d.TIME != 7 AND d.TIME != 8) ORDER BY DATE, YEAR, MONTH, DAY, TIME, category, dong ASC")
+
+# x_train = x_train.reshape(x_train.shape[0], x_train.shape[1],1,1)
+# x_pred = x_pred.reshape(x_pred.shape[0], x_pred.shape[1],1,1)
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train,  train_size=0.9, random_state = 77, shuffle=True ) 
+
+print(x_train.shape, y_train.shape) #(2213481, 46, 1) (2213481,)
+print(x_pred.shape, y_pred.shape)   #(177408, 46, 1) (177408,)
+print(x_val.shape, y_val.shape)     #(245943, 46, 1) (245943,) 
+
+model = ak.StructuredDataRegressor(max_trials=3, loss='mean_absolute_error')
+model.fit(x_train, y_train, epochs=10, validation_data=(x_val, y_val))
+
+loss = model.evaluate(x_pred, y_pred)
+print(loss)
+
+
